83ce2a9defd247365a3b4bd9ea4d7b7966f00924
a-powers <awpowers@gmail.com>
2021-02-17 18:29:18 +0000

    update

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import math
 def paint_calc(height, width, cover):
     coverage = (height*width)/cover
     calc = math.ceil(coverage)
-    print(calc)
+    print(f"You will need {calc} cans of paint.")
 
 
 test_h = int(input("Height of wall: "))
 test_w = int(input("Width of wall: "))
 coverage = 5
 
-paint_calc(height=test_h, width=test_w, cover=coverage)
+paint_calc(test_h,test_w,coverage)
